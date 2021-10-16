0d5df6c9187aed55c273c0e6bf8f476c48ea8fd8
Edmund Lewry <edmund.lewry@ntlworld.com>
2021-10-16 21:52:05 +0100

    Had to change the Graph data structures to records rather than structs in order for the Yaml deserializer to be able to initially construct the empty objects

--- Graph.cs
+++ Graph.cs
@@ -2,7 +2,23 @@
 
 namespace svg_graph_builder
 {
-    public record GraphCollection(List<Graph> Graphs);
-    public record Graph(string Type, string Title, string XAxisLabel, string YAxisLabel, List<GraphDatum> Data);
-    public record GraphDatum(object X, object Y);
+    public struct GraphCollection
+    {
+        public List<Graph> Graphs { get; init; }
+    }
+
+    public struct Graph
+    {
+        public string Type { get; init; }
+        public string Title {get; init; }
+        public string XAxisLabel {get; init; }
+        public string YAxisLabel {get; init; }
+        public List<GraphDatum> Data {get; init; }
+    }
+
+    public struct GraphDatum
+    {
+        public object X { get; init; }
+        public object Y { get; init; }
+    }
 }
